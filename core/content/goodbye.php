fa2537861c99a2b65ed89293792144c2d5f72d6f
Bram Van Rensbergen <mail@bramvanrensbergen.com>
2014-10-29 11:39:54 +0100

    Removed redundant php code.

--- core/content/goodbye.php
+++
@@ -1,9 +0,0 @@
-<div id = "goodbye" class = "section" data-anchor = "-1>
-
-	<h1>Klaar!</h1>
-	<p>Het experiment is afgelopen.</p>
-	<p>Bedankt voor je deelname!</p>
-	
-	<p>Je mag dit venster nu sluiten.</p>
-
-</div>
