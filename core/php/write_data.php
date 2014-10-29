ec2ff63004a27836a9e77da78a1eb19ca13ea905
Bram Van Rensbergen <mail@bramvanrensbergen.com>
2014-10-29 11:41:24 +0100

    Updated output path.

--- core/php/write_data.php
+++ core/php/write_data.php
@@ -1,12 +1,11 @@
 <?php 
-	
 	if ($_POST["append"]) {
 		$mode = "a";
 	} else {
 		$mode = "w";
 	} 
 	
- 	$fh = fopen("../output/".$_POST["filename"], $mode);
+ 	$fh = fopen("../../output/".$_POST["filename"], $mode);
 	fwrite($fh, $_POST["data"]);
     fclose($fh); 
 ?>
